0500a7964c49cd8bdc94b59e0927141eea209af9
Xiaojia <annacl1822@gmail.com>
2023-06-19 16:57:53 -0700

    create filters

---
+++ app/utils/filters.py
@@ -0,0 +1,13 @@
+# Formats a date object as MM/DD/YY
+def format_date(date):
+    return date.strftime('%m/%d/%y')
+
+# Only includes domain name using replace and split 
+def format_url(url): 
+    return url.replace('http://', '').replace('https://', '').repalce('www.', '')[0].split('?')[0]
+
+def format_plural(amount, word): 
+    if amount != 1: 
+        return word + 's'
+    
+    return word 
